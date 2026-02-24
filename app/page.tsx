337c21a541ca5b8f60b4cab0e11d3574c615c62b
CristinaJung <cristijung@gmail.com>
2026-02-24 15:48:48 -0300

    query e redux

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Counter from "@/components/counter/Counter";
 import ThemeApp from "@/components/themeApp/ThemeApp";
 import ToDo from "../components/todo/Todo";
 import Cats from "@/components/cats/Cats";
+import BreedCatalog from "@/components/cats/BreedCatalog";
+import PostList from "@/components/postList/PostList";
 
 export default function Home() {
   return (
@@ -10,6 +12,8 @@ export default function Home() {
       <Counter />
       <ToDo />
       <Cats />
+      <BreedCatalog />
+      <PostList />
     </>
   );
 }
--- store/store.ts
+++ store/store.ts
@@ -3,6 +3,8 @@ import themeReducer from './themeSlice';
 import countReducer from './counterSlice';
 import todoReducer from './todoSlice';
 import { catApi } from "./catApi";
+import { catApiDois } from "./catApiDois";
+import { postApi } from "./postApi";
 
 
 export const store = configureStore({
@@ -11,10 +13,16 @@ export const store = configureStore({
         counter: countReducer,
         todo: todoReducer,
         [catApi.reducerPath]: catApi.reducer,
+        [catApiDois.reducerPath]: catApiDois.reducer,
+        [postApi.reducerPath]: postApi.reducer,
     },
 
     middleware: (getDefaultMiddleware) => 
-        getDefaultMiddleware().concat(catApi.middleware), 
+        getDefaultMiddleware().concat(
+            catApi.middleware,
+            catApiDois.middleware,
+            postApi.middleware
+        ), 
 });
 
 export type RootState = ReturnType<typeof store.getState>;
